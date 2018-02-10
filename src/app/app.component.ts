79de794ef4e341971785c6c9899645766e679bc2
JamesChe91 <semajsva91@gmail.com>
2018-02-10 11:50:01 -0500

    adding event keyboard listener

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { map } from 'rxjs/operators';
 //Import firebase dependencies
 import { AngularFireDatabase } from 'angularfire2/database';
+
 
 @Component({
   selector: 'app-root',
@@ -13,8 +14,20 @@ export class AppComponent {
   items: Observable<any[]>;
   title = 'Angular Lab #1';
 
-  constructor(db: AngularFireDatabase) {
-    this.items = db.list('users').valueChanges()
+  constructor(private db: AngularFireDatabase) {
+  }
+  ngOnInit() {
+  }
+  @HostListener('window:keyup', ['$event'])
+  keyEvent(event: KeyboardEvent) {
+    console.log(event);
+    console.log(event.keyCode)
+    if (event.keyCode === 32) {
+      this.getProfile();
+    }
+  }
+  getProfile() {
+  this.items = this.db.list('users').valueChanges()
     .pipe(map(pickRandomItem));
   }
 }
