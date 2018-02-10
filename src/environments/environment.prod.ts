abd4e3cf5b623e13d9bd07cfe0b60b21695a1308
JamesChe91 <semajsva91@gmail.com>
2018-02-10 11:14:46 -0500

    Getting random data

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC1JYTVCPdNPhkRCZ0alllZTiKhBO8vWRM",
+    authDomain: "angular-lab-42365.firebaseapp.com",
+    databaseURL: "https://angular-lab-42365.firebaseio.com",
+    projectId: "angular-lab-42365",
+    storageBucket: "angular-lab-42365.appspot.com",
+    messagingSenderId: "785279765613"
+  }
 };
